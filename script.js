132bc4c7d273ff2fde13c66841bdfed17c3471cb
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-09 23:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const input=document.getElementById('input')
+const bottons=document.querySelectorAll('button')
+
+
+bottons.forEach((button)=>{
+	button.addEventListener('click',()=>{
+
+		if(button.id==='clear'){
+			input.value=''
+		}
+
+		else if(button.id==='ans'){
+
+			try{
+				input.value=eval(input.value)
+			}
+			catch(error){
+				
+			}
+		}
+
+		else{
+			input.value+=button.textContent
+		}
+
+		
+		
+	})
+})
